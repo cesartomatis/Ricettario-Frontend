4e0e77bd48b5f77cf9d0f849a00c016c87625d08
Cesar Tomatis <cesartomatis@outlook.com>
2020-03-23 18:06:15 +0100

    added a no tips label

--- src/components/Recipe/RecipeTips/RecipeTips.js
+++ src/components/Recipe/RecipeTips/RecipeTips.js
@@ -6,10 +6,15 @@ import { I18nContext } from '../../../i18n';
 const RecipeTips = (props) => {
 	const { translate } = useContext(I18nContext);
 
+	let tips = translate('NO_TIPS');
+	if (props.tips && props.tips !== '') {
+		tips = props.tips;
+	}
+
 	return (
 		<div className={classes.RecipeTipsContainer}>
 			<label className={classes.TipsTitle}>{translate('RECIPE_TIPS')}</label>
-			<label className={classes.TipsText}>{props.tips}</label>
+			<label className={classes.TipsText}>{tips}</label>
 		</div>
 	);
 };
